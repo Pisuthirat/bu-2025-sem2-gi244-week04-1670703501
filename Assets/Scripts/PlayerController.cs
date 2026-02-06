daf56817a1194d2389d7d2d9779e6ca686454f5e
Pisuthirat <Pisuthirat.kham@bumail.net>
2026-02-06 16:26:00 +0700

    wip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,61 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10;
+    public float xRange = 10;
+
+    public GameObject foodPrefab;
+
+    private InputAction moveAction;
+    private InputAction shootAction;
+
     private void Awake()
     {
-
+        moveAction = InputSystem.actions.FindAction("Move");
+        shootAction = InputSystem.actions.FindAction("Shoot");
     }
 
     // Update is called once per frame
     void Update()
     {
+        var hInput = moveAction.ReadValue<Vector2>().x;
+        transform.Translate(hInput * speed * Time.deltaTime * Vector3.right);
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange,
+                transform.position.y,
+                transform.position.z);
+        }
+        else if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange,
+            transform.position.y,
+            transform.position.z);
+        }
 
+        if (shootAction.triggered)
+        {
+            Instantiate(foodPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //Gizmos.DrawWireSphere(transform.position, 1f);
+        //Gizmos.color = Color.purple;
+        //Gizmos.DrawLine(transform.position, Camera.main.transform.position);
+
+        Vector3 Left = new Vector3(-xRange,
+            transform.position.x,
+            transform.position.z);
+        Vector3 right = new Vector3(xRange,
+            transform.position.y,
+            transform.position.z);
+        Gizmos.DrawLine(Left, right);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(Left, 0.5f);
+        Gizmos.DrawSphere(right, 0.5f);
     }
 }
